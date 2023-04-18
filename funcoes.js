c1d1dc27afc7df9892e4546b0934076403cddef1
Emerson <emersonianbezerra@gmail.com>
2023-04-17 21:50:15 -0300

    Alterar Caminhos

--- funcoes.js
+++ funcoes.js
@@ -1,9 +1,9 @@
 function redir(){
-    window.location.href = "file:///C:/Users/emers/Documents/Cadastro-Proles-main/Cadastro-Proles-main/formulario.html"
+    window.location.href = "https://emersonianb.github.io/CadastroProles/formulario.html"
 }
 
 function redir_ent(){
-    window.location.href = "file:///C:/Users/emers/Documents/Cadastro-Proles-main/Cadastro-Proles-main/entrar.html"
+    window.location.href = "https://emersonianb.github.io/Banco_Proles/entrar.html"
 }
 
 function redir_conc(){

7748d4a95cdfe79339e68f821a985efac7ff8303
Emerson <emersonianbezerra@gmail.com>
2023-04-17 21:39:34 -0300

    Update

---
+++ funcoes.js
@@ -0,0 +1,11 @@
+function redir(){
+    window.location.href = "file:///C:/Users/emers/Documents/Cadastro-Proles-main/Cadastro-Proles-main/formulario.html"
+}
+
+function redir_ent(){
+    window.location.href = "file:///C:/Users/emers/Documents/Cadastro-Proles-main/Cadastro-Proles-main/entrar.html"
+}
+
+function redir_conc(){
+    window.location.href = "file:///C:/Users/emers/Documents/Cadastro-Proles-main/Cadastro-Proles-main/inicial.html"
+}

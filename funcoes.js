edd754628a3ccb2c15e40090857883212b415778
Emerson <emersonianbezerra@gmail.com>
2023-05-11 13:32:46 -0300

    update

--- funcoes.js
+++ funcoes.js
@@ -3,9 +3,28 @@ function redir(){
 }
 
 function redir_ent(){
-    window.location.href = "https://emersonianb.github.io/Banco_Proles/entrar.html"
+    window.location.href = "http://127.0.0.1:3000/entrar.html"
 }
 
 function redir_conc(){
     window.location.href = "file:///C:/Users/emers/Documents/Cadastro-Proles-main/Cadastro-Proles-main/inicial.html"
+}
+
+var radio = document.querySelector('.manual-bnt1')
+var cont = 1
+
+document.getElementById('radio1').checked = true
+
+setInterval(() => {
+    proximaImg()
+}, 5000)
+
+function proximaImg(){
+    cont++
+
+    if (cont > 3){
+        cont = 1
+    }
+
+    document.getElementById('radio'+cont).checked = true
 }
